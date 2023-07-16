0c922eea30e4115bde4faddd8e5265a5471d0faa
lucas.sampaio <lucas.impulso@parceiro.gruposaltaedu.com>
2023-07-16 18:52:03 -0300

    ajustado context para criar as migrations corretamente

--- ImportadorCNAB.Infra/ClienteContext.cs
+++ ImportadorCNAB.Infra/ClienteContext.cs
@@ -6,33 +6,35 @@ namespace ImportadorCNAB.Infra;
 
 public class ClienteContext : DbContext, IUnitOfWork
 {
+    public ClienteContext(DbContextOptions<ClienteContext> options) : base(options)
+    {
+    }
+
     public DbSet<Cliente> Clientes { get; set; }
     public DbSet<Transacao> Transacoes { get; set; }
     public DbSet<TipoTransacao> TiposTransacoes { get; set; }
+    public DbSet<TransacaoPositiva> TiposTransacoesPositivas { get; set; }
+    public DbSet<TransacaoNegativa> TiposTransacoesNegativas { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         //usado so em debug para obter erros mais detalhados
-        #if DEBUG
+#if DEBUG
         optionsBuilder.EnableDetailedErrors();
-        #endif
+#endif
 
         optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTrackingWithIdentityResolution);
 
         base.OnConfiguring(optionsBuilder);
     }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        //seta as colunas string pra varchar(100)
-        var propriedades = modelBuilder.Model.GetEntityTypes().SelectMany(x => x.GetProperties().Where(y => y.ClrType == typeof(string)));
-        foreach (var property in propriedades)
-        {
-            property.SetColumnType("varchar(100)");
-        }
         // pega as configurações das entidades
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClienteContext).Assembly);
         base.OnModelCreating(modelBuilder);
     }
+
     protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
     {
         configurationBuilder
@@ -41,6 +43,7 @@ public class ClienteContext : DbContext, IUnitOfWork
 
         base.ConfigureConventions(configurationBuilder);
     }
+
     public async ValueTask<bool> CommitAsync()
     {
         var sucesso = await base.SaveChangesAsync() > 0;
