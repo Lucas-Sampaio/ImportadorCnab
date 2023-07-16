74115441ad9bd813bb26372ecbba9ad5152f2ea6
lucas.sampaio <lucas.impulso@parceiro.gruposaltaedu.com>
2023-07-16 14:22:58 -0300

    ajuste config tipoTrasancao

--- ImportadorCNAB.Infra/EntityConfigurations/TipoTransacaoConfig.cs
+++ ImportadorCNAB.Infra/EntityConfigurations/TipoTransacaoConfig.cs
@@ -27,5 +27,23 @@ public class TipoTransacaoConfig : IEntityTypeConfiguration<TipoTransacao>
 
         builder.HasDiscriminator(x => x.Natureza)
             .HasValue<TransacaoNegativa>(ENatureza.Saida);
+
+        builder.HasData(ObterTransacoesIniciais());
+    }
+
+    private static List<TipoTransacao> ObterTransacoesIniciais()
+    {
+        return new List<TipoTransacao>()
+        {
+            new TransacaoPositiva(1,"Debito"),
+            new TransacaoNegativa(2,"Boleto"),
+            new TransacaoNegativa(3,"Financiamento"),
+            new TransacaoPositiva(4,"Crédito"),
+            new TransacaoPositiva(5,"Recebimento Empréstimo"),
+            new TransacaoPositiva(6,"Vendas"),
+            new TransacaoPositiva(7,"Recebimento TED"),
+            new TransacaoPositiva(8,"Recebimento DOC"),
+            new TransacaoNegativa(9,"Aluguel"),
+        };
     }
 }
