b0fd3eca016f4e34d44ee665357134913889c5cb
lucas.sampaio <lucas.impulso@parceiro.gruposaltaedu.com>
2023-07-16 18:53:19 -0300

    add id no construtor como opcional pq para criar os dados iniciais preciso forncer um id

--- ImportadorCNAB.Domain/ClienteAggregate/TransacaoNegativa.cs
+++ ImportadorCNAB.Domain/ClienteAggregate/TransacaoNegativa.cs
@@ -2,8 +2,15 @@
 
 public class TransacaoNegativa : TipoTransacao
 {
-    public TransacaoNegativa(int codigo, string descricao) : base(codigo, descricao)
+    /// <summary>
+    /// Constroi um objeto TransacaoNegativa
+    /// </summary>
+    /// <param name="codigo"> codigo</param>
+    /// <param name="descricao">descricao </param>
+    /// <param name="id">Opcional - permite criar com o valor id inicial</param>
+    public TransacaoNegativa(int codigo, string descricao, int id = 0) : base(codigo, descricao)
     {
         this.Natureza = Shared.Enums.ENatureza.Saida;
+        this.Id = id;
     }
 }
--- ImportadorCNAB.Domain/ClienteAggregate/TransacaoPositiva.cs
+++ ImportadorCNAB.Domain/ClienteAggregate/TransacaoPositiva.cs
@@ -2,8 +2,15 @@
 
 public class TransacaoPositiva : TipoTransacao
 {
-    public TransacaoPositiva(int codigo, string descricao) : base(codigo, descricao)
+    /// <summary>
+    /// Constroi um objeto TransacaoPositiva
+    /// </summary>
+    /// <param name="codigo"> codigo</param>
+    /// <param name="descricao">descricao </param>
+    /// <param name="id">Opcional - permite criar com o valor id inicial</param>
+    public TransacaoPositiva(int codigo, string descricao, int id = 0) : base(codigo, descricao)
     {
         this.Natureza = Shared.Enums.ENatureza.Entrada;
+        this.Id = id;
     }
 }
