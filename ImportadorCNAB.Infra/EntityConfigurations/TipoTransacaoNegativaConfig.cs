c13d02e6b310540d8c55dc5794ed16fdcf1af6e6
lucas.sampaio <lucas.impulso@parceiro.gruposaltaedu.com>
2023-07-16 18:11:44 -0300

    config tipo transacao

---
+++ ImportadorCNAB.Infra/EntityConfigurations/TipoTransacaoNegativaConfig.cs
@@ -0,0 +1,23 @@
+﻿using ImportadorCNAB.Domain.ClienteAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ImportadorCNAB.Infra.EntityConfigurations;
+
+public class TipoTransacaoNegativaConfig : IEntityTypeConfiguration<TransacaoNegativa>
+{
+    public void Configure(EntityTypeBuilder<TransacaoNegativa> builder)
+    {
+         builder.HasData(ObterTransacoesIniciais());
+    }
+
+    private static List<TipoTransacao> ObterTransacoesIniciais()
+    {
+        return new List<TipoTransacao>()
+        {
+            new TransacaoNegativa(2,"Boleto"),
+            new TransacaoNegativa(3,"Financiamento"),
+            new TransacaoNegativa(9,"Aluguel")
+        };
+    }
+}
---
+++ ImportadorCNAB.Infra/EntityConfigurations/TipoTransacaoPositivaConfig.cs
@@ -0,0 +1,26 @@
+﻿using ImportadorCNAB.Domain.ClienteAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ImportadorCNAB.Infra.EntityConfigurations;
+
+public class TipoTransacaoPositivaConfig : IEntityTypeConfiguration<TransacaoPositiva>
+{
+    public void Configure(EntityTypeBuilder<TransacaoPositiva> builder)
+    {
+        builder.HasData(ObterTransacoesIniciais());
+    }
+
+    private static List<TipoTransacao> ObterTransacoesIniciais()
+    {
+        return new List<TipoTransacao>()
+        {
+            new TransacaoPositiva(1,"Debito"),
+            new TransacaoPositiva(4,"Crédito"),
+            new TransacaoPositiva(5,"Recebimento Empréstimo"),
+            new TransacaoPositiva(6,"Vendas"),
+            new TransacaoPositiva(7,"Recebimento TED"),
+            new TransacaoPositiva(8,"Recebimento DOC"),
+        };
+    }
+}
