2cebe3b8167f22cd428248d5628a568231b3a8cb
lucas.sampaio <lucas.impulso@parceiro.gruposaltaedu.com>
2023-07-17 22:34:46 -0300

    adicionado mais testes

--- ImportadorCNAB.Tests/DomainTests/ClienteTestes.cs
+++ ImportadorCNAB.Tests/DomainTests/ClienteTestes.cs
@@ -23,4 +23,24 @@ public class ClienteTestes
         Assert.Equal(cpf, cliente.Cpf);
         Assert.NotNull(cliente);
     }
+
+    [Trait("Domain", "Cliente teste")]
+    [Fact(DisplayName = nameof(Cliente_Adiciona_Transacao_com_sucesso))]
+    public void Cliente_Adiciona_Transacao_com_sucesso()
+    {
+        //Arrange
+        var nomeloja = "nomeLoja";
+        var nome = "nome";
+        var cpf = new CPF("961.232.190-65");
+        var transacao = new Transacao(DateTimeOffset.UtcNow, 10, "123456", new TransacaoPositiva(1, "teste"));
+        //Act
+        var cliente = new Cliente(nomeloja, nome, cpf);
+        cliente.AdicionarTransacao(transacao);
+        //Assert
+        Assert.Equal(nome, cliente.Nome);
+        Assert.Equal(nomeloja, cliente.NomeLoja);
+        Assert.Equal(cpf, cliente.Cpf);
+        Assert.NotNull(cliente);
+        Assert.NotEmpty(cliente.Transacoes);
+    }
 }
