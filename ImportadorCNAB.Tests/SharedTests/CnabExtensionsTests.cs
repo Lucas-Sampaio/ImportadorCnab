8eec33097e67e111a9df3692cc756dc0f4225441
lucas.sampaio <lucas.impulso@parceiro.gruposaltaedu.com>
2023-07-19 00:33:34 -0300

    criado testes para o shared

---
+++ ImportadorCNAB.Tests/SharedTests/CnabExtensionsTests.cs
@@ -0,0 +1,101 @@
+﻿using ImportadorCNAB.Shared.Utils;
+using Xunit;
+
+namespace ImportadorCNAB.Tests.SharedTests;
+
+public class CnabExtensionsTests
+{
+    private const string linhaCnab = "3201903010000014200096206760174753****3153153453JOÃO MACEDO   BAR DO JOÃO  ";
+
+    [Trait("Shared", "Cnab teste")]
+    [Fact(DisplayName = nameof(Cnab_Obter_tipo_com_sucesso))]
+    public void Cnab_Obter_tipo_com_sucesso()
+    {
+        //Arrange
+
+        //Act
+        var valor = linhaCnab.CnabObterTipo();
+
+        //Assert
+        Assert.Equal("3", valor);
+    }
+
+    [Trait("Shared", "Cnab teste")]
+    [Fact(DisplayName = nameof(Cnab_Obter_valor_com_sucesso))]
+    public void Cnab_Obter_valor_com_sucesso()
+    {
+        //Arrange
+
+        //Act
+        var valor = linhaCnab.CnabObterValor();
+
+        //Assert
+        Assert.Equal("0000014200", valor);
+    }
+
+    [Trait("Shared", "Cnab teste")]
+    [Fact(DisplayName = nameof(Cnab_Obter_cpf_com_sucesso))]
+    public void Cnab_Obter_cpf_com_sucesso()
+    {
+        //Arrange
+
+        //Act
+        var valor = linhaCnab.CnabObterCpf();
+
+        //Assert
+        Assert.Equal("09620676017", valor);
+    }
+
+    [Trait("Shared", "Cnab teste")]
+    [Fact(DisplayName = nameof(Cnab_Obter_Cartao_com_sucesso))]
+    public void Cnab_Obter_Cartao_com_sucesso()
+    {
+        //Arrange
+
+        //Act
+        var valor = linhaCnab.CnabObterCartao();
+
+        //Assert
+        Assert.Equal("4753****3153", valor);
+    }
+
+    [Trait("Shared", "Cnab teste")]
+    [Fact(DisplayName = nameof(Cnab_Obter_Cartao_com_sucesso))]
+    public void Cnab_Obter_nome_dono_loja_com_sucesso()
+    {
+        //Arrange
+
+        //Act
+        var valor = linhaCnab.CnabObterNomeDonoLoja();
+
+        //Assert
+        Assert.Equal("JOÃO MACEDO", valor);
+    }
+
+    [Trait("Shared", "Cnab teste")]
+    [Fact(DisplayName = nameof(Cnab_Obter_nome_loja_com_sucesso))]
+    public void Cnab_Obter_nome_loja_com_sucesso()
+    {
+        //Arrange
+
+        //Act
+        var valor = linhaCnab.CnabObterNomeLoja();
+
+        //Assert
+        Assert.Equal("BAR DO JOÃO", valor);
+    }
+
+    [Trait("Shared", "Cnab teste")]
+    [Fact(DisplayName = nameof(Cnab_Obter_Data_com_sucesso))]
+    public void Cnab_Obter_Data_com_sucesso()
+    {
+        //Arrange
+
+        var data = new DateTimeOffset(2019, 3, 1, 15, 34, 53, TimeSpan.FromHours(-3));
+        //Act
+        var valor = linhaCnab.CnabObterData();
+
+        //Assert
+        Assert.Equal(data.LocalDateTime, valor.LocalDateTime);
+    }
+}
---
+++ ImportadorCNAB.Tests/SharedTests/StringExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using ImportadorCNAB.Shared.Utils;
+using Xunit;
+
+namespace ImportadorCNAB.Tests.SharedTests;
+
+public class StringExtensionsTests
+{
+    [Trait("Shared", "String extensions teste")]
+    [Fact(DisplayName = nameof(Converter_string_para_inteiro_realizado_sucesso))]
+    public void Converter_string_para_inteiro_realizado_sucesso()
+    {
+        //Arrange
+
+        //Act
+        var valor = "4".ToInt32();
+
+        //Assert
+        Assert.Equal(4, valor);
+    }
+
+    [Trait("Shared", "String extensions teste")]
+    [Fact(DisplayName = nameof(Converter_string_para_inteiro_com_valor_default_realizado_sucesso))]
+    public void Converter_string_para_inteiro_com_valor_default_realizado_sucesso()
+    {
+        //Arrange
+        var valorDefault = 6;
+        string? numString = null; 
+        //Act
+        var valor = numString.ToInt32(valorDefault);
+
+        //Assert
+        Assert.Equal(valorDefault, valor);
+    }
+
+    [Trait("Shared", "String extensions teste")]
+    [Fact(DisplayName = nameof(Converter_string_para_decimal_realizado_sucesso))]
+    public void Converter_string_para_decimal_realizado_sucesso()
+    {
+        //Arrange
+
+        //Act
+        var valor = "4,8".ToDecimal();
+
+        //Assert
+        Assert.Equal(4.8M, valor);
+    }
+
+    [Trait("Shared", "String extensions teste")]
+    [Fact(DisplayName = nameof(Converter_string_para_decimal_com_valor_default_realizado_sucesso))]
+    public void Converter_string_para_decimal_com_valor_default_realizado_sucesso()
+    {
+        //Arrange
+        var valorDefault = 6.8M;
+        string? numString = null;
+        //Act
+        var valor = numString.ToDecimal(valorDefault);
+
+        //Assert
+        Assert.Equal(valorDefault, valor);
+    }
+}
