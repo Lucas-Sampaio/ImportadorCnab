bdfa7a6c282ac54b9094b26d49186cab2d3b38c3
lucas.sampaio <lucas.impulso@parceiro.gruposaltaedu.com>
2023-07-18 21:25:45 -0300

    criado controller cliente

---
+++ ImportadorCNAB.Api/Controllers/ClienteController.cs
@@ -0,0 +1,34 @@
+﻿using ImportadorCNAB.Api.Application.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImportadorCNAB.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ClienteController : ControllerBase
+{
+    private readonly IClienteQuery _clienteQuery;
+
+    public ClienteController(IClienteQuery clienteQuery)
+    {
+        _clienteQuery = clienteQuery;
+    }
+
+    [HttpGet("ObterLojas")]
+    public async Task<IActionResult> GetLojas(CancellationToken cancellation)
+    {
+        var lojas = await _clienteQuery.ObterLojas(cancellation);
+
+        return Ok(lojas);
+    }
+
+    [HttpGet("{clienteId}")]
+    public async Task<IActionResult> GetCliente(int clienteId, CancellationToken cancellation)
+    {
+        var cliente = await _clienteQuery.ObterCliente(clienteId, cancellation);
+        if (cliente is null)
+            return NotFound();
+
+        return Ok(cliente);
+    }
+}
