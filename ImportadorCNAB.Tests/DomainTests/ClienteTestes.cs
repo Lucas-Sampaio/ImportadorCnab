08d94aa7d01cf2a1f143b421c6e3f35a0f180afb
lucas.sampaio <lucas.impulso@parceiro.gruposaltaedu.com>
2023-07-19 00:33:43 -0300

    criado testes pro domain

--- ImportadorCNAB.Tests/DomainTests/ClienteTestes.cs
+++ ImportadorCNAB.Tests/DomainTests/ClienteTestes.cs
@@ -32,15 +32,57 @@ public class ClienteTestes
         var nomeloja = "nomeLoja";
         var nome = "nome";
         var cpf = new CPF("961.232.190-65");
-        var transacao = new Transacao(DateTimeOffset.UtcNow, 10, "123456", new TransacaoPositiva(1, "teste"));
-        //Act
+        var transacao1 = new Transacao(DateTimeOffset.UtcNow, 10, "123456", new TransacaoPositiva(1, "teste"));
+        var transacao2 = new Transacao(DateTimeOffset.UtcNow, 10, "123456", new TransacaoNegativa(2, "testeN"));
         var cliente = new Cliente(nomeloja, nome, cpf);
-        cliente.AdicionarTransacao(transacao);
+
+        //Act
+        cliente.AdicionarTransacao(transacao1);
+        cliente.AdicionarTransacao(transacao2);
+
         //Assert
         Assert.Equal(nome, cliente.Nome);
         Assert.Equal(nomeloja, cliente.NomeLoja);
         Assert.Equal(cpf, cliente.Cpf);
         Assert.NotNull(cliente);
         Assert.NotEmpty(cliente.Transacoes);
+        Assert.Equal(2, cliente.Transacoes.Count);
+        Assert.Collection(cliente.Transacoes,
+            item => Assert.IsType<TransacaoPositiva>(item.TipoTransacao),
+            item => Assert.IsType<TransacaoNegativa>(item.TipoTransacao));
+    }
+
+    [Trait("Domain", "Cliente teste")]
+    [Fact(DisplayName = nameof(Cliente_Calcula_valor_saldo_certo))]
+    public void Cliente_Calcula_valor_saldo_certo()
+    {
+        //Arrange
+        var nomeloja = "nomeLoja";
+        var nome = "nome";
+        var cpf = new CPF("961.232.190-65");
+        var valorP1 = 100;
+        var valorP2 = 200;
+        var valorN1 = 100;
+        var valorN2 = 50.80M;
+        var totalEsperado = valorP1 + valorP2 - (valorN1 + valorN2);
+        var transacao1 = new Transacao(DateTimeOffset.UtcNow, valorP1, "123456", new TransacaoPositiva(1, "teste"));
+        var transacao2 = new Transacao(DateTimeOffset.UtcNow, valorP2, "123456", new TransacaoPositiva(1, "teste"));
+        var transacao3 = new Transacao(DateTimeOffset.UtcNow, valorN1, "123456", new TransacaoNegativa(2, "testeN"));
+        var transacao4 = new Transacao(DateTimeOffset.UtcNow, valorN2, "123456", new TransacaoNegativa(2, "testeN"));
+        var cliente = new Cliente(nomeloja, nome, cpf);
+
+        //Act
+        cliente.AdicionarTransacao(transacao1);
+        cliente.AdicionarTransacao(transacao2);
+        cliente.AdicionarTransacao(transacao3);
+        cliente.AdicionarTransacao(transacao4);
+
+        //Assert
+        Assert.Equal(nome, cliente.Nome);
+        Assert.Equal(nomeloja, cliente.NomeLoja);
+        Assert.Equal(cpf, cliente.Cpf);
+        Assert.NotNull(cliente);
+        Assert.NotEmpty(cliente.Transacoes);
+        Assert.Equal(totalEsperado, cliente.ObterValorTotalSaldo());
     }
 }
--- ImportadorCNAB.Tests/DomainTests/TransacaoTestes.cs
+++ ImportadorCNAB.Tests/DomainTests/TransacaoTestes.cs
@@ -7,8 +7,8 @@ namespace ImportadorCNAB.Tests.DomainTests;
 public class TransacaoTestes
 {
     [Trait("Domain", "Transacao teste")]
-    [Fact(DisplayName = nameof(Transacao_Construido_com_sucesso))]
-    public void Transacao_Construido_com_sucesso()
+    [Fact(DisplayName = nameof(TransacaoPositiva_Construido_com_sucesso))]
+    public void TransacaoPositiva_Construido_com_sucesso()
     {
         //Arrange
         var data = DateTimeOffset.UtcNow;
@@ -24,6 +24,29 @@ public class TransacaoTestes
         Assert.Equal(valor, transacao.Valor);
         Assert.Equal(cartao, transacao.CartaoUtilizadoNumero);
         Assert.Equal(tipo.Codigo, transacao.TipoTransacao.Codigo);
+        Assert.IsType<TransacaoPositiva>(transacao.TipoTransacao);
+        Assert.NotNull(transacao);
+    }
+
+    [Trait("Domain", "Transacao teste")]
+    [Fact(DisplayName = nameof(TransacaoNegativa_Construido_com_sucesso))]
+    public void TransacaoNegativa_Construido_com_sucesso()
+    {
+        //Arrange
+        var data = DateTimeOffset.UtcNow;
+        var valor = 10;
+        var cartao = "152128498";
+        var tipo = new TransacaoNegativa(2, "boleto");
+
+        //Act
+        var transacao = new Transacao(data, valor, cartao, tipo);
+
+        //Assert
+        Assert.Equal(data, transacao.Data);
+        Assert.Equal(valor, transacao.Valor);
+        Assert.Equal(cartao, transacao.CartaoUtilizadoNumero);
+        Assert.Equal(tipo.Codigo, transacao.TipoTransacao.Codigo);
+        Assert.IsType<TransacaoNegativa>(transacao.TipoTransacao);
         Assert.NotNull(transacao);
     }
 }
